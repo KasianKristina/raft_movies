cea276a2b86048547538e36a5344854ebd5f8a47
Kasian Kristina <kasyankn@gmail.com>
2025-04-22 21:20:21 +0300

    fix styles, add button styles

--- src/stories/Button.svelte
+++ src/stories/Button.svelte
@@ -2,26 +2,21 @@
 	import './button.css';
 
 	interface Props {
-		/** Is this the principal call to action on the page? */
-		primary?: boolean;
 		/** What background color to use */
 		backgroundColor?: string;
-		/** How large should the button be? */
-		size?: 'small' | 'medium' | 'large';
 		/** Button contents */
 		label: string;
 		/** The onclick event handler */
 		onClick?: () => void;
 	}
 
-	const { primary = false, backgroundColor, size = 'medium', label, onClick }: Props = $props();
+	const { backgroundColor, label, onClick }: Props = $props();
 </script>
 
 <button
 	type="button"
-	class={['button', `button--${size}`].join(' ')}
-	class:button--primary={primary}
-	class:button--secondary={!primary}
+	class={'button'}
+	class:button--primary={false}
 	style:background-color={backgroundColor}
 	onclick={onClick}
 >
